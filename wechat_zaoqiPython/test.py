38e8981e3928a30e20e5373d8073c5b6856beeb4
jingnanzh <jingnanzh0417@gmail.com>
2020-10-21 14:20:07 -0600

    trail 3

--- wechat_zaoqiPython/test.py
+++ wechat_zaoqiPython/test.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import pandas as pd
+import random
 
 url = 'https://www.bilibili.com/v/popular/rank/all?spm_id_from=333.851.b_7072696d61727950616765546162.3'
 res = requests.get(url)
